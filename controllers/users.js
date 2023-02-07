806d8ec1d3268cef28577ddf2880f0db7c9557ac
Mihail Majukin <m.v.majukin@mail.ru>
2023-02-08 01:23:45 +0300

    Исправить controllers/users

--- controllers/users.js
+++ controllers/users.js
@@ -3,39 +3,68 @@ const User = require("../models/user");
 module.exports.getUsers = (req, res) => {
   User.find({})
     .then((users) => res.send(users))
-    .catch(() => res.status(404).send({ message: "Пользователь не найден" }));
+    .catch((err) => res.status(500).send({ message: err.message }));
 };
 
 module.exports.getUser = (req, res) => {
   User.findById(req.params.userId)
-    .then((user) => res.send(user))
-    .catch(() => res.status(404).send({ message: "id пользователя не найден" }));
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send({ message: "Пользователь не найден" });
+      }
+      return res.send(user);
+    })
+    .catch((err) => {
+      if (err.name === "CastError") {
+        return res
+          .status(400)
+          .send({ message: "id пользователя не найден" });
+      }
+      return res.status(500).send({ message: err.message });
+    });
 };
 
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
 
   User.create({ name, about, avatar })
-    .then((users) => res.send(users))
-    .catch(() => res.status(400).send({ message: "Переданы некорректные данные в метод создания пользователя" }));
+    .then((user) => res.send(user))
+    .catch((err) => {
+      if (err.name === "ValidationError") {
+        return res.status(400).send({
+          message: "Переданы некорректные данные в метод создания пользователя",
+        });
+      }
+      return res.status(500).send({ message: err.message });
+    });
 };
 
 module.exports.updateProfile = (req, res) => {
   const { name, about } = req.body;
 
-  User.findByIdAndUpdate(req.user._id, { name, about })
-    .then((user) => res.send({
-      _id: user._id, avatar: user.avatar, name, about,
-    }))
-    .catch(() => res.status(400).send({ message: "Переданы некорректные данные в метод обновления профиля" }));
+  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
+    .then((user) => res.send({ user }))
+    .catch((err) => {
+      if (err.name === "ValidationError") {
+        return res.status(400).send({
+          message: "Переданы некорректные данные в метод обновления профиля",
+        });
+      }
+      return res.status(500).send({ message: err.message });
+    });
 };
 
 module.exports.updateAvatar = (req, res) => {
   const { avatar } = req.body;
 
-  User.findByIdAndUpdate(req.user._id, { avatar })
-    .then((user) => res.send({
-      _id: user._id, avatar, name: user.name, about: user.about,
-    }))
-    .catch(() => res.status(400).send({ message: "Переданы некорректные данные в метод обновления аватара" }));
+  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
+    .then((user) => res.send({ user }))
+    .catch((err) => {
+      if (err.name === "ValidationError") {
+        return res.status(400).send({
+          message: "Переданы некорректные данные в метод обновления аватар",
+        });
+      }
+      return res.status(500).send({ message: err.message });
+    });
 };
