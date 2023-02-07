5ce5af037b82d806939c14cef5adf3760534a2ff
Mihail Majukin <m.v.majukin@mail.ru>
2023-02-08 01:27:04 +0300

    Исправить controllers/cards

--- controllers/cards.js
+++ controllers/cards.js
@@ -3,7 +3,7 @@ const Card = require("../models/card");
 module.exports.getCards = (req, res) => {
   Card.find({})
     .then((cards) => res.send(cards))
-    .catch(() => res.status(404).send({ message: "Карточка не найдена" }));
+    .catch((err) => res.status(500).send({ message: err.message }));
 };
 
 module.exports.createCard = (req, res) => {
@@ -11,13 +11,34 @@ module.exports.createCard = (req, res) => {
   const owner = req.user._id;
   Card.create({ name, link, owner })
     .then((card) => res.send(card))
-    .catch(() => res.status(400).send({ message: "Переданы некорректные данные в метод создания карточки" }));
+    .catch((err) => {
+      if (err.name === "ValidationError") {
+        return res.status(400).send({
+          message: "Переданы некорректные данные в метод создания карточки",
+        });
+      }
+      return res.status(500).send({ message: err.message });
+    });
 };
 
 module.exports.deleteCard = (req, res) => {
   Card.findByIdAndRemove(req.params.cardId)
-    .then((card) => res.send(card))
-    .catch(() => res.status(404).send({ message: "id карточки не найден" }));
+    .then((card) => {
+      if (!card) {
+        return res.status(404).send({
+          message: "Карточки не существует",
+        });
+      }
+      return res.send(card);
+    })
+    .catch((err) => {
+      if (err.name === "CastError") {
+        return res.status(400).send({
+          message: "id карточки не найден",
+        });
+      }
+      return res.status(500).send({ message: err.message });
+    });
 };
 
 module.exports.likeCard = (req, res) => {
@@ -26,8 +47,22 @@ module.exports.likeCard = (req, res) => {
     { $addToSet: { likes: req.user._id } },
     { new: true }
   )
-    .then((card) => res.send(card))
-    .catch(() => res.status(404).send({ message: "Карточка не найдена" }));
+    .then((card) => {
+      if (!card) {
+        return res.status(404).send({
+          message: "Карточка не найдена",
+        });
+      }
+      return res.send(card);
+    })
+    .catch((err) => {
+      if (err.name === "CastError") {
+        return res.status(400).send({
+          message: "id карточки не найден",
+        });
+      }
+      return res.status(500).send({ message: err.message });
+    });
 };
 
 module.exports.dislikeCard = (req, res) => {
@@ -36,6 +71,20 @@ module.exports.dislikeCard = (req, res) => {
     { $pull: { likes: req.user._id } },
     { new: true }
   )
-    .then((card) => res.send(card))
-    .catch(() => res.status(404).send({ message: "Карточка не найдена" }));
+    .then((card) => {
+      if (!card) {
+        return res.status(404).send({
+          message: "Карточка не найдена",
+        });
+      }
+      return res.send(card);
+    })
+    .catch((err) => {
+      if (err.name === "CastError") {
+        return res.status(400).send({
+          message: "id карточки не найден",
+        });
+      }
+      return res.status(500).send({ message: err.message });
+    });
 };
